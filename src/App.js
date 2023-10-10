620d9d635b447abb4d2853a2918303441f5950b0
Vovababiichuk <babihuk888@gmail.com>
2023-10-10 17:08:00 +0300

    added Footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import React from 'react';
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home } from './pages';
+import SigninPage from './pages/signin';
 
 function App() {
-  return (
-    <Router>
-      <Home />
-    </Router>
-  );
+   return (
+      <Router>
+         <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signin" element={<SigninPage />} />
+         </Routes>
+      </Router>
+   );
 }
 
 export default App;
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import { HeroSection } from '../components/HeroSection';
 import { InfoSection } from '../components/InfoSection';
 import { homeObjOne, homeObjTwo, homeObjThree } from '../components/InfoSection/Data';
 import { Services } from '../components/Services';
+import { Footer } from '../components/Footer';
 
 export const Home = () => {
    const [isOpen, setIsOpen] = useState(false);
@@ -24,6 +25,7 @@ export const Home = () => {
          <InfoSection {...homeObjTwo} />
          <Services />
          <InfoSection {...homeObjThree} />
+         <Footer />
       </Fragment>
    );
 };
